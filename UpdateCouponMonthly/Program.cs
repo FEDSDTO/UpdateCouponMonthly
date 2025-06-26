08d678c936e72188538684fd3bc3818a462806c9
HQFEG\feds02424 <pojhihchen@feds.com.tw>
2025-06-26 14:56:57 +0800

    每月最後一天回寫UsedRule兌出起訖時間 每月第一天寄報表給會計

--- UpdateCouponMonthly/Program.cs
+++ UpdateCouponMonthly/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace UpdateCouponMonthly
 {
@@ -10,6 +11,19 @@ namespace UpdateCouponMonthly
     {
         static void Main(string[] args)
         {
+            if (DateTime.Now.Day == DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)) //當月最後一天
+            {
+                UpdateCoupon.UpdateExchangeTime();
+                UpdateCoupon.UpdateSAP_B_Time();
+            }
+            if (DateTime.Now.Day == 1) //當月第一天
+            {
+                CommonUtility.MoveFiles();
+                if (ConfigurationManager.AppSettings["SendMail"] == "Y")
+                {
+                    CommonUtility.SendMail();
+                }
+            }
         }
     }
 }
